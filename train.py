db91b4a855559eefdd2a6eab6c18d67fd51c38ae
Muhammad Ahsan <69692532+Muhammad-Ahsan-Rasheed@users.noreply.github.com>
2023-01-05 11:02:29 +0500

    Update train.py

--- train.py
+++ train.py
@@ -1,90 +1,136 @@
-# 2. Predict.py
-# Predict flower name from an image with predict.py along with the probability of that name. That is, you'll pass in a single image /path/to/image and return the flower name and class probability.
+# 1. Train
+# Train a new network on a data set with train.py
 
-# Basic usage: python predict.py /path/to/image checkpoint
+# Basic usage: python train.py data_directory
+# Prints out training loss, validation loss, and validation accuracy as the network trains
 # Options:
-# Return top KK most likely classes: python predict.py input checkpoint --top_k 3
-# Use a mapping of categories to real names: python predict.py input checkpoint --category_names cat_to_name.json
-# Use GPU for inference: python predict.py input checkpoint --gpu
+# Set directory to save checkpoints: python train.py data_dir --save_dir save_directory
+# Choose architecture: python train.py data_dir --arch "vgg13"
+# Set hyperparameters: python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
+# Use GPU for training: python train.py data_dir --gpu
 
-# Imports here
-import argparse
 import torch
-import torchvision
-from torchvision import datasets, transforms, models
 from torch import nn
 from torch import optim
 import torch.nn.functional as F
-from collections import OrderedDict
-from PIL import Image
+from torchvision import datasets, transforms, models
+import matplotlib.pyplot as plt
 import numpy as np
-import json
+from PIL import Image
+import seaborn as sns
+import argparse
+from collections import OrderedDict
 
-# Define command line arguments for predict.py
-def get_input_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input', type=str, help='Input image')
-    parser.add_argument('checkpoint', type=str, help='Checkpoint file')
-    parser.add_argument('--top_k', type=int, default=3, help='Return top K most likely classes')
-    parser.add_argument('--category_names', type=str, default='', help='Mapping of categories to real names')
-    parser.add_argument('--gpu', action='store_true', help='Use GPU for inference')
-    args = parser.parse_args()
-    return args
+def argparser():
+    parser = argparse.ArgumentParser(description='Train.py')
+    parser.add_argument('data_dir', action='store', default='flowers')
+    parser.add_argument('--save_dir', action='store', dest='save_dir', default='checkpoint.pth')
+    parser.add_argument('--arch', action='store', dest='arch', default='vgg13')
+    parser.add_argument('--learning_rate', action='store', dest='learning_rate', type=float, default=0.001)
+    parser.add_argument('--hidden_units', action='store', dest='hidden_units', type=int, default=512)
+    parser.add_argument('--epochs', action='store', dest='epochs', type=int, default=20)
+    parser.add_argument('--gpu', action='store', dest='gpu', default='gpu')
+    return parser.parse_args()
 
-def process_image(image):
-    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
-        returns an Numpy array
-    '''
-    pil_image = Image.open(image)
-    pil_image.thumbnail((256, 256))
-    pil_image = pil_image.crop((16, 16, 240, 240))
-    np_image = np.array(pil_image)
-    np_image = np_image/255
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    np_image = (np_image - mean)/std
-    np_image = np_image.transpose((2, 0, 1))
-    return np_image
+def train():
 
+    args = argparser()
+    
+    data_dir = args.data_dir
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
+    test_dir = data_dir + '/test'
 
-def predict():
-    # Get input arguments
-    args = get_input_args()
-    # Load checkpoint
-    checkpoint = torch.load(args.checkpoint)
-    # Load category names
-    if args.category_names:
-        with open(args.category_names, 'r') as f:
-            cat_to_name = json.load(f)
-    # Load model
-    model = getattr(models, checkpoint['arch'])(pretrained=True)
-    model.classifier = checkpoint['classifier']
-    model.load_state_dict(checkpoint['state_dict'])
-    model.class_to_idx = checkpoint['class_to_idx']
-    # Process image
-    image = Image.open(args.input)
-    image = process_image(image)
-    # Predict
-    image = torch.from_numpy(image).type(torch.FloatTensor)
-    image = image.unsqueeze_(0)
-    if args.gpu:
-        image = image.cuda()
-        model = model.cuda()
-    model.eval()
-    with torch.no_grad():
-        output = model.forward(image)
-    ps = torch.exp(output)
-    top_p, top_class = ps.topk(args.top_k, dim=1)
-    top_p = top_p[0].tolist()
-    top_class = top_class[0].tolist()
-    if args.category_names:
-        top_class = [cat_to_name[str(i+1)] for i in top_class]
-        top_flowers = [cat_to_name[str(i+1)] for i in top_class]
-    return top_p, top_class, top_flowers
+    data_transforms =  transforms.Compose([transforms.RandomRotation(30), transforms.RandomResizedCrop(224), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
+    image_datasets = datasets.ImageFolder(train_dir, transform=data_transforms)
 
+    dataloaders = torch.utils.data.DataLoader(image_datasets, batch_size=64, shuffle=True)
 
+    model = getattr(models, args.arch)(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    split = args.hidden_units
+
+    classifier = nn.Sequential(OrderedDict([
+                            ('fc1', nn.Linear(25088, split)),
+                            ('relu', nn.ReLU()),
+                            ('dropout', nn.Dropout(p=0.5)),
+                            ('fc2', nn.Linear(split, 102)),
+                            ('output', nn.LogSoftmax(dim=1))
+                            ]))
+
+    model.classifier = classifier
+
+    criterion = nn.NLLLoss()
+
+    optimizer = optim.Adam(model.classifier.parameters(), lr=args.learning_rate)
+
+    device = torch.device("cuda" if torch.cuda.is_available() and args.gpu else "cpu")
+    print('Available device is : ', device)
+
+    model.to(device)       
+
+    epochs = args.epochs
+    steps = 0
+    running_loss = 0
+    print_every = 5
+
+    for epoch in range(epochs):
+        for inputs, labels in dataloaders:
+            steps += 1
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in dataloaders:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+
+                        test_loss += batch_loss.item()
+
+                        # Calculate accuracy
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+                print(f"Epoch {epoch+1}/{epochs}.. "
+                      f"Train loss: {running_loss/print_every:.3f}.. "
+                      f"Test loss: {test_loss/len(dataloaders):.3f}.. "
+                      f"Test accuracy: {accuracy/len(dataloaders):.3f}")
+                running_loss = 0
+                model.train()
+
+    model.class_to_idx = image_datasets.class_to_idx
+
+    checkpoint = {'input_size': 25088,
+                    'output_size': 102,
+                    'arch': args.arch,
+                    'learning_rate': args.learning_rate,
+                    'hidden_units': args.hidden_units,
+                    'epochs': args.epochs,
+                    'classifier': classifier,
+                    'state_dict': model.state_dict(),
+                    'class_to_idx': model.class_to_idx}
+                    
+    torch.save(checkpoint, args.save_dir)
+
+    
 if __name__ == '__main__':
-    top_p, top_class = predict()
-    print(top_p)
-    print(top_class)
+    train()
